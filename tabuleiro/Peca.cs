51a336300b6326cd037c86b93dd61d02160fc469
Gustavo de Lima <gustavodelima1997@outlook.com>
2019-09-17 22:22:38 -0300

    adicionando classes

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,52 @@
+﻿
+namespace tabuleiro
+{
+    abstract class Peca
+    {
+
+        public Posicao posicao { get; set; }
+        public Cor cor { get; set; }
+        public int qtdMovimentos { get; protected set; }
+        public Tabuleiro tab { get; set; }
+
+        public Peca(Cor cor, Tabuleiro tab)
+        {
+            this.posicao = null;
+            this.cor = cor;
+            this.tab = tab;
+            this.qtdMovimentos = 0;
+        }
+
+        public void incrementarQtdMovimentos()
+        {
+            qtdMovimentos++;
+        }
+
+        public void decrementarQtdMovimentos()
+        {
+            qtdMovimentos--;
+        }
+
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for ( int i=0; i<tab.linhas; i++)
+            {
+                for( int j=0; j<tab.colunas; j++)
+                {
+                    if(mat[i,j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool movimentoPossivel(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+        public abstract bool[,] movimentosPossiveis();
+    }
+}
